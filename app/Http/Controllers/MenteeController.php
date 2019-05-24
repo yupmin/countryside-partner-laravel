107f0f8af03e24e491e39d10744a72763ab7363f
Young-jin Yun <yupmin@gmail.com>
2019-05-25 00:13:21 +0900

    - MenteeService 는 좀더 모듈화가 필요할때하세요. - 인터페이스도 필요로 할때 만드세요. - ide-helper:generate, meta, model 를 실행하고, 관련 파일들을 .gitignore 에 추가하세요.

--- app/Http/Controllers/MenteeController.php
+++ app/Http/Controllers/MenteeController.php
@@ -4,39 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreMenteeRequest;
 use App\Models\Mentee;
-use App\Services\MenteeInterface;
-use App\Services\MenteeService;
-use Illuminate\Http\Request;
-use JWTAuth;
+use App\Services\FileUploadService;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class MenteeController extends Controller
 {
+    /** @var FileUploadService */
+    private $fileUploadService;
 
-    private $mentee;
-
-    public function __construct(MenteeInterface $mentee)
+    public function __construct(FileUploadService $fileUploadService)
     {
-        $this->mentee = $mentee;
+        $this->fileUploadService = $fileUploadService;
     }
 
+    protected function store(StoreMenteeRequest $request)
+    {
+        $data = $request->all();
 
-    /**
-     * @param StoreMenteeRequest $request
-     * @return mixed
-     */
-    protected function store(StoreMenteeRequest $request){
+        $data['profile_image'] = $this->fileUploadService->profileUpload($request->file('profile_image'));
+        $mentee = Mentee::create($data);
+        $mentee->setAttribute('token', JWTAuth::fromUser($mentee));
 
-        $menteeInfo = $this->mentee->create($request);
-
-        return response()->success($menteeInfo);
-
+        return response()->success($mentee);
     }
 
-
-    /**
-     * @return mixed
-     */
-    protected function lists(){
+    protected function lists()
+    {
 
         $mentees = Mentee::all();
         return response()->success($mentees);
