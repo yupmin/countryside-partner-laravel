815c637471a2d880394f3e3796c7494fcbf19db1
Young-jin Yun <yupmin@gmail.com>
2019-05-24 23:53:06 +0900

    모델을 굳이 DI 할 필요는 없습니다.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,17 +7,12 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-
-    private $mentor = null;
-
-    public function __construct(Mentor $mentor)
-    {
-        $this->mentor = $mentor;
-    }
-
     protected function index()
     {
-        $mentors = $this->mentor::inRandomOrder()->limit(8)->get();
-        return response()->success($mentors);
+        $mentors = Mentor::inRandomOrder()
+            ->limit(8)
+            ->get();
+
+        return $mentors;
     }
 }
